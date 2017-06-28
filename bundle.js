6145fa90c17dd1d8c1c3513ca30144890704ce39
lanetteammonika <lanetteam.monikaa@gmail.com>
2017-06-28 14:48:14 +0530

    adding files

---
+++ bundle.js
@@ -0,0 +1,61 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	import * as mortgage from './mortgage';
+	
+	document.getElementById('calcBtn').addEventListener('click', function () {
+	    let principal = document.getElementById("principal").value;
+	    let years = document.getElementById("years").value;
+	    let rate = document.getElementById("rate").value;
+	    let {monthlyPayment, monthlyRate, amortization} = mortgage.calculateAmortization(principal, years, rate);
+	    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+	    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+	    amortization.forEach(month => console.log(month));
+	});
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
